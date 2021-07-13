33a880676fe932e5f924cc0bc8378bf412e731f9
mohan-vaishnavi <46642908+mohan-vaishnavi@users.noreply.github.com>
2021-07-13 22:06:29 +0530

    Update Pair.cs

--- TelCo.ColorCoder/Pair.cs
+++ TelCo.ColorCoder/Pair.cs
@@ -7,40 +7,41 @@ namespace TelCo.ColorCoder
 	class Pair
 	{
 		public Color majorColor;
-        	public Color minorColor;
+        public Color minorColor;
 		public override string ToString()
 		{
 		    return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
 		}
+		
+		public static int GetColorIndex(Color[] colorArray, Color color)
+		{
+			int index = -1;
+			for (int i = 0; i < colorArray.Length; i++)
+				    {
+					if (colorArray[i] == color)
+					{
+					    index = i;
+					    break;
+					}
+				    }
+			return index;
+		}
+		
         public static int GetPairNumberFromColor(Pair pair)
         {
             // Find the major color in the array and get the index
-            int majorIndex = -1;
-            for (int i = 0; i < getColor.colorMapMajor.Length; i++)
-            {
-                if (getColor.colorMapMajor[i] == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
+            int majorIndex = GetColorIndex(GetColor.colorMapMajor, pair.majorColor);
+            
             // Find the minor color in the array and get the index
-            int minorIndex = -1;
-            for (int i = 0; i < getColor.colorMapMinor.Length; i++)
-            {
-                if (getColor.colorMapMinor[i] == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
+            int minorIndex = GetColorIndex(GetColor.colorMapMinor, pair.minorColor);
+            
             // If colors can not be found throw an exception
             if (majorIndex == -1 || minorIndex == -1)
             {
                 throw new ArgumentException(
                     string.Format("Unknown Colors: {0}", pair.ToString()));
             }
-            return (majorIndex * getColor.colorMapMinor.Length) + (minorIndex + 1);
+            return (majorIndex * GetColor.colorMapMinor.Length) + (minorIndex + 1);
         }
 
     }
