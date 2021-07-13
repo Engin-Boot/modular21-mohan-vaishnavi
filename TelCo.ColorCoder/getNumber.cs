3e3e4d28d0375ab732b4590166019924c7b1d616
mohan-vaishnavi <46642908+mohan-vaishnavi@users.noreply.github.com>
2021-07-13 15:47:24 +0530

    Update getNumber.cs

--- TelCo.ColorCoder/getNumber.cs
+++ TelCo.ColorCoder/getNumber.cs
@@ -10,11 +10,18 @@ namespace TelCo.ColorCoder
 		public static Color[] colorMapMajor;
 
         public static Color[] colorMapMinor;
+		
+	
 
         static getNumber()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+		
+	public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
         }
 
         public static int GetPairNumberFromColor(ColorPair pair)
