4c09719dbfdcbd5f492e1b20c674d0ca79e7076e
mohan-vaishnavi <46642908+mohan-vaishnavi@users.noreply.github.com>
2021-07-13 17:04:49 +0530

    Update Pair.cs

--- TelCo.ColorCoder/Pair.cs
+++ TelCo.ColorCoder/Pair.cs
@@ -6,9 +6,6 @@ namespace TelCo.ColorCoder
 {  
 	class Pair
 	{
-		public static Color[] colorMapMajor;
-        	public static Color[] colorMapMinor;
-				
 		public override string ToString()
 		{
 		    return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
